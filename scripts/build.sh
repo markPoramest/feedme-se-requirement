8d2d0eff707c93c930f85fd521985f8cbdce813e
Mark Poramest <2411kjn@gmail.com>
2026-04-15 11:57:18 +0700

    fix: build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,12 +7,11 @@ echo "Building McDonald's Order Management System..."
 echo ""
 
 # Build the application
-cd ..
-go build -o bin/feedme main.go
+go build main.go
 
 if [ $? -eq 0 ]; then
-  echo "Build successful! Binary created at bin/feedme"
-  echo "To run the application, execute: ./bin/feedme"
+  echo "Build successful! Binary created at root "
+  echo "To run the application, execute: main"
 else
   echo "Build failed!"
   exit 1
